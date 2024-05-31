7430eb3fd030d4cd2d1a6865e79f2a2c85a703e6
Laryd <hillarymuboka@gmail.com>
2024-05-30 23:21:07 -0700

    feat(auth): add SessionProvider to wrap entire app
    Added a SessionWrapper component to provide session context throughout the entire application.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { cn } from "../lib/utils";
 import { NavBar } from "@/components/NavBar";
+import SessionWrapper from "@/components/SessionWrapper";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,16 +18,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={cn(
-          "min-h-screen font-sans antialiased grainy",
-          inter.className
-        )}
-      >
-        <NavBar/>
-        {children}
-      </body>
-    </html>
+    <SessionWrapper>
+      <html lang="en">
+        <body
+          className={cn(
+            "min-h-screen font-sans antialiased grainy",
+            inter.className
+          )}
+        >
+          <NavBar />
+          {children}
+        </body>
+      </html>
+    </SessionWrapper>
   );
 }
