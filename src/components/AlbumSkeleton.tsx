ce1b89f42ff8cc36d5c68753cd10e4a3a4b2106a
Laryd <hillarymuboka@gmail.com>
2024-06-04 11:41:57 -0700

    feat(ui):add optional width prop to Album Skeleton
    In order to change width components that don't have similar default width

--- src/components/AlbumSkeleton.tsx
+++ src/components/AlbumSkeleton.tsx
@@ -1,16 +1,16 @@
 import Skeleton from "react-loading-skeleton";
 
-const AlbumSkeleton = ({ cards }: { cards: number }) => {
+const AlbumSkeleton = ({ cards, width }: { cards: number, width?:number }) => {
   return Array(cards)
     .fill(0)
     .map((item) => (
       <div>
         <div className="top-row">
-          <Skeleton width={150} style={{marginBottom: "1.7rem"
+          <Skeleton width={width ? width : 150} style={{marginBottom: "1.7rem"
           }} />
         </div>
         <div className="bottom-row rounded-lg">
-          <Skeleton height={400} />
+          <Skeleton height={300} />
         </div>
       </div>
     ));
