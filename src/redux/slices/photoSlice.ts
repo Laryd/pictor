60613558ed40708f0a63ad9f2719f3f20d66ed4a
Laryd <hillarymuboka@gmail.com>
2024-06-10 04:42:12 -0700

    refactor(photoSlice): add selector for update title status

--- src/redux/slices/photoSlice.ts
+++ src/redux/slices/photoSlice.ts
@@ -15,7 +15,7 @@ interface PhotoState {
   singlePhoto: Photo | null;
   status: "idle" | "loading" | "succeeded" | "failed";
   singlePhotoStatus: "idle" | "loading" | "succeeded" | "failed";
-  updateStatus: "idle" | "loading" | "succeeded" | "failed"; 
+  updateStatus: "idle" | "loading" | "succeeded" | "failed";
 }
 
 const initialState: PhotoState = {
@@ -23,7 +23,7 @@ const initialState: PhotoState = {
   singlePhoto: null,
   status: "idle",
   singlePhotoStatus: "idle",
-  updateStatus: "idle", 
+  updateStatus: "idle",
 };
 
 export const fetchPhotos = createAsyncThunk("photos/fetchPhotos", async () => {
@@ -102,19 +102,21 @@ const photoSlice = createSlice({
           if (state.singlePhoto?.id === action.payload.id) {
             state.singlePhoto = action.payload;
           }
-          state.updateStatus = "succeeded"; 
+          state.updateStatus = "succeeded";
         }
       )
       .addCase(updatePhotoTitle.rejected, (state) => {
-        state.updateStatus = "failed"; 
+        state.updateStatus = "failed";
       });
   },
 });
 
 export const selectPhotosStatus = (state: RootState) => state.photos.status;
-export const selectSinglePhotoStatus = (state: RootState) => state.photos.singlePhotoStatus;
+export const selectSinglePhotoStatus = (state: RootState) =>
+  state.photos.singlePhotoStatus;
+export const selectUpdateTitleStatus = (state: RootState) =>
+  state.photos.updateStatus;
 export const selectPhotos = (state: RootState) => state.photos.photos;
 export const selectSinglePhoto = (state: RootState) => state.photos.singlePhoto;
-
 
 export default photoSlice.reducer;
